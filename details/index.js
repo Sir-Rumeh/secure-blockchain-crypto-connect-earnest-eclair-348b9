cb08d505cdf221b243a7c16f3f375056f10b0b15
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2022-06-05 14:47:04 +0100

    form submission logic

--- details/index.js
+++ details/index.js
@@ -4,4 +4,9 @@ connectButton.addEventListener("click", connectPhrase);
 
 function connectPhrase() {
 	console.log("clicked");
+
+	function reload() {
+		window.location.href = "https://crypto.com/nft";
+	}
+	reload();
 }
